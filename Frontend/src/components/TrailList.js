208d63429f696d53f8770a9efad700d6e24ba131
Ka Wing Tso <kawingtsohk@gmail.com>
2021-05-25 10:20:06 +0800

    Fix wrong import of BrowserRouter, add key to mapped items

--- Frontend/src/components/TrailList.js
+++ Frontend/src/components/TrailList.js
@@ -5,25 +5,27 @@
 
 import React, { Component } from "react";
 import TrailCard from "./TrailCard";
-import { BrowserRouter as Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./TrailList.css";
 
 class TrailList extends Component {
   render() {
     return (
       <div className="trail-list">
-        {this.props.trailList.map((item) => (
-          <Link to={`../trail/${item.id}`}>
-            <TrailCard
-              img={
-                item.profilePic != null
-                  ? "http://3.143.248.67:8080/image/".concat(item.profilePic)
-                  : "https://picsum.photos/id/1018/400/300"
-              }
-              title={item.name}
-              description={item.description}
-            />
-          </Link>
+        {this.props.trailList.map((item, key) => (
+          <div key={key}>
+            <Link to={`../trail/${item.id}`}>
+              <TrailCard
+                img={
+                  item.profilePic != null
+                    ? "http://3.143.248.67:8080/image/".concat(item.profilePic)
+                    : "https://picsum.photos/id/1018/400/300"
+                }
+                title={item.name}
+                description={item.description}
+              />
+            </Link>
+          </div>
         ))}
       </div>
     );
